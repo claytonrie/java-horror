154d0df242c7981de76950bc0b360d1772221b63
Clayton Riegger <jonfieldthe2nd@gmail.com>
2016-01-10 18:13:11 -0500

    Change formatting to have brackets on the same line

--- java-horror-story/src/Main.java
+++ java-horror-story/src/Main.java
@@ -1,9 +1,7 @@
 
-public class Main
-{
+public class Main {
 
-	public static void main(String[] args)
-	{
+	public static void main(String[] args) {
 		System.out.println("AAAAAAAAAGH THE HORROR!");
 	}
 
